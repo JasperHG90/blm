25e3ea766c604551c144a2a8f9d0f66c107381ac
Jasper Ginn <jasperginn@gmail.com>
2019-03-25 09:42:31 +0100

    Converting PPC to julia

--- blm/inst/julia/ppc_blm.jl
+++ blm/inst/julia/ppc_blm.jl
@@ -22,8 +22,10 @@ function ppc_draws(X::Array{Float64}, y::Array{Float64}, w::Array{Float64},
     # 4. Set up a results matrix for simulated yhat values
     res = Array{Float64}(undef, iterations - burn, size(X)[1])
 
-    # 5. Set up a results matrix for residuals
+    # 5. Set up a results matrix for residuals, skewness stats and heteroskedasticity stats
     resids = Array{Float64}(undef, iterations - burn, size(X)[1], 2)
+    skewed = Array{Float64}(undef, iterations - burn, 2)
+    heterosked = Array{Float64}(undef, iterations - burn, 2)
 
     # 5. Draw from a normal distribution given each specific y value and populate results matrix
     for j in 1:(iterations - burn)
@@ -37,12 +39,22 @@ function ppc_draws(X::Array{Float64}, y::Array{Float64}, w::Array{Float64},
         # Compute residuals (observed)
         resids[j, :, 2] = y .- lincom[:, j]
 
+        # Adjusted R^2 values for simulated & observed data
+        heterosked[j, 1] = adjR2(X, resids[j, :, 1].^2)
+        heterosked[j, 2] = adjR2(X, resids[j, :, 2].^2))
+
+        # Skewness for simulated & observed data
+        skewed[j, 1] = skewness(resids[j, :, 1])
+        skewed[j, 2] = skewness(resids[j, :, 2])
+
         end;
 
     # 6. Return
     return(Dict(
       "sim_y" => res,
-      "residuals" => resids
+      "residuals" => resids,
+      "heteroskedasticity" => heterosked,
+      "skewness" => skewness
     ))
 
     end;
@@ -73,3 +85,48 @@ function simulate_y(computed_y::Array{Float64}, sd::Float64)
     return(res)
 
     end;
+
+function predict_y(X,y)
+
+  #=
+  Predict outcome variable y
+  =#
+
+  # Linear model, prediction and return
+  return(X * (inv( transpose(X) * X ) * transpose(X) * y))
+
+  end;
+
+function adjR2(X, y)
+
+  #=
+  Calculate sums of squares & R2
+  =#
+
+  TSS::Float64
+  RSS::Float64
+  R2::Float64
+
+  # TSS
+  TSS = sum((y .- mean(y)).^2)
+
+  # RSS
+  RSS = sum( (predict_y(X,y) .- y).^2 )
+
+  # R2
+  R2 = 1 - (RSS / TSS)
+
+  # Adjusted R2
+  return( 1 - ((1 - R2) * (size(X)[1] - 1)) / (size(X)[1] - (size(X)[2]-1) - 1) )
+
+  end;
+
+function skewness(x)
+
+  #=
+  Calculate skewness in vector standardized vector x
+  =#
+
+  return((sum(x^3) / size(x)[1]) / ((sum(x^2) / size(x)[1])^1.5))
+
+  end;
